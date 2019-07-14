94b437a6f5233d4b59edf5f594803fbcfe8febc7
njorothegniuos <njosh51@gmail.com>
2019-07-14 17:24:05 +0300

    First commit

---
+++ src/main/java/com/DotConsumerService/ConsumerService.java
@@ -0,0 +1,5 @@
+package com.DotConsumerService;
+
+public class ConsumerService {
+
+}
---
+++ src/main/java/com/DotConsumerService/DotConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.DotConsumerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotConsumerServiceApplication.class, args);
+	}
+
+}
